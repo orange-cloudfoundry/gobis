89dd9ba45607a26cab6913a371b8a4e93344e1ee
ArthurHlt <arthur.halet@orange.com>
2017-07-23 15:55:29 +0200

    can forge a route path

--- proxy_route.go
+++ proxy_route.go
@@ -117,7 +117,7 @@ func (r *ProxyRoute) LoadParams() {
 	r.AppPath = reg.FindStringSubmatch(r.Path)[1]
 	r.Url = strings.TrimSuffix(r.Url, "/")
 }
-func (r ProxyRoute) CreateRoutePath(finalPath string) {
+func (r ProxyRoute) CreateRoutePath(finalPath string) string {
 	reg := regexp.MustCompile(PATH_REGEX)
 	sub := reg.FindStringSubmatch(r.Path)
 	return sub[1] + finalPath
